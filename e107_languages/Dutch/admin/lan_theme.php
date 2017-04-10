ce612fe5562438aa974943d90d28d7b0ef117b0f
leo <the_guardian2@hotmail.com>
2017-04-10 21:45:08 +0200

    Update lan_theme.php
    Upd. 10-04-17

--- e107_languages/Dutch/admin/lan_theme.php
+++ e107_languages/Dutch/admin/lan_theme.php
@@ -129,4 +129,5 @@ define("TPVLAN_89", "Gebruik deze instellingen voor alle beheerders");
 define("TPVLAN_90","Kon geen verbinding maken, controleer firewall en/of internet verbinding.");
 define("TPVLAN_91", "Maak een nieuw thema gebaseerd op");
 define("TPVLAN_92", "Nieuwe thema map");
+define("TPVLAN_93", "Selectie");
 ?>
