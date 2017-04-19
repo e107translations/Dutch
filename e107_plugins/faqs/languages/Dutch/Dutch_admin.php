521da890466f5db25eac37b14e3b1c97dc99485c
leo <the_guardian2@hotmail.com>
2017-04-20 00:46:27 +0200

    Update Dutch_admin.php
    updated 20-04-17

--- e107_plugins/faqs/languages/Dutch/Dutch_admin.php
+++ e107_plugins/faqs/languages/Dutch/Dutch_admin.php
@@ -12,6 +12,7 @@
 
 define("LANA_FAQ_QUESTION", "Vraag");
 define("LANA_FAQ_ANSWER", "Antwoord");
+define("LANA_FAQ_QUESTIONS", "Vragen");
 define("LANA_FAQ_UNANSWERED", "Nog niet beantwoord");
 define("LANA_FAQ_COMMENT", "Reactie klasse");
 //define("LANA_FAQ_UNAME", "Gebruikersnaam");
@@ -23,6 +24,26 @@ define("LANA_FAQ_METAK", "Meta trefwoorden");
 define("LANA_FAQ_PREF_1", "Sta het plaatsen van FAQs toe aan :");
 define("LANA_FAQ_PREF_2", "Sta het plaatsen van Vragen toe aan:");
 define("LANA_FAQ_PREF_3", "Gebruik klassieke weergave");
-
-
+define("LANA_FAQ_PREF_4", "'Stel een vraag' limiet per gebruiker");
+define("LANA_FAQ_PREF_5", "'Stel een vraag' karakter limiet");
+define("LANA_FAQ_PREF_6", "'Stel een vraag' beperkt toto");
+define("LANA_FAQ_PREF_7", "Gestelde vragen bevestigd");
+define("LANA_FAQ_PREF_8", "Lijst Type");
+define("LANA_FAQ_PREF_9", "Ongeordende lijst");
+define("LANA_FAQ_PREF_10", "Geordende lijst");
+define("LANA_FAQ_PREF_11", "Pagina Titel");
+define("LANA_FAQ_PREF_12", "'Nieuw' FAQs zijn niet meer dan");
+define("LANA_FAQ_PREF_13", "Toon FAQ totaal");
+define("LANA_FAQ_PREF_14", "Toon datumstempel");
+define("LANA_FAQ_PREF_15", "Toon sociale knoppen");
+define("LANA_FAQ_PREF_16", "Gespecificeerde volgorde");
+define("LANA_FAQ_PREF_17", "Alle talen");
+define("LANA_FAQ_PREF_HELP_1", "0 = geen limiet");
+define("LANA_FAQ_PREF_HELP_2", "Vul niets in om standaard waarde te gebruiken");
+define("LANA_FAQ_CRON_1", "Niet beantwoorde vragen rapport");
+define("LANA_FAQ_CRON_2", "Verstuur een mail met overzicht van niet beantwoorde ragen naar [x].");
+define("LANA_FAQ_CRON_3", "[x] niet beantwoorde vragen in [y]");
+define("LANA_FAQ_CRON_4", "Login op je website om deze vragen te bekijken en te beantwoorden met onderstaande link.");
+define("LANA_FAQ_CRON_5", "De [x] meest recente vragen worden hieronder getoond.");
+define("LANA_FAQ_CRON_6", "[x] niet beantwoorde vragen vanaf  [y].");
 ?>
