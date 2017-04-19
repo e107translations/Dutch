1fc424f19077bfbd35d7a2d79003cc08a4cd706b
leo <the_guardian2@hotmail.com>
2017-04-20 01:03:25 +0200

    Update Dutch_admin.php
    typefout

--- e107_plugins/tinymce4/langs/Dutch_admin.php
+++ e107_plugins/tinymce4/langs/Dutch_admin.php
@@ -11,7 +11,7 @@
 */
 define("TMCEALAN_1", "Plak als tekst is standaard");
 define("TMCEALAN_2", "Browser spellingscontrole");
-define("TMCEALAN_3", "Gebruik dit wanner de interne spellingscontrole van de browser moet worden gebruikt.");
+define("TMCEALAN_3", "Gebruik dit wanneer de interne spellingscontrole van de browser moet worden gebruikt.");
 define("TMCEALAN_4", "Visuele Blokken");
 define("TMCEALAN_5", "Activeer dit om html blokken zichtbaar te maken tijdens het wijzigen.");
 ?>
