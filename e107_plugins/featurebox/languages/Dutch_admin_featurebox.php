1a3dc2a6c28b894b9d7c4b34e7351d83f467a257
leo <the_guardian2@hotmail.com>
2017-06-12 19:34:40 +0200

    Create Dutch_admin_featurebox.php
    12-06-17

--- e107_plugins/featurebox/languages/Dutch_admin_featurebox.php
+++ e107_plugins/featurebox/languages/Dutch_admin_featurebox.php
@@ -17,6 +17,15 @@ define("FBLAN_14", "Toon uitsluitend dit bericht");
 define("FBLAN_22", "Weergave methode");
 define("FBLAN_23", "In thema blok");
 define("FBLAN_24", "Normaal");
+define("FBLAN_25", "Aanmaken Categorie");
+define("FBLAN_26", "Afbeelding/Video"); 
+define("FBLAN_27", "Afbeeldingslink"); 
+define("FBLAN_28", "Dingenblok Menu Categorie"); 
+define("FBLAN_29", "Categorie voor gebruik door dingenblok menu"); 
+define("FBLAN_30", "Categorie template"); 
+define("FBLAN_31", "Willekeurig"); 
+define("FBLAN_32", "Parameters (optioneel)"); 
+define("FBLAN_33", "Optionele Javascript Parameters (formaat onder voorbehoud wijziging)");
 
 
 ?>
