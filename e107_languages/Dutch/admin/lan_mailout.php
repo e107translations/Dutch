fd965a80760e3e1c20ad35759a7d5f7d0499ca27
leo <the_guardian2@hotmail.com>
2017-10-15 14:48:30 +0200

    Update lan_mailout.php
    revoked change 15-10-2017

--- e107_languages/Dutch/admin/lan_mailout.php
+++ e107_languages/Dutch/admin/lan_mailout.php
@@ -218,6 +218,7 @@ define("LAN_MAILOUT_205", "Email verzendings-formaat fout");
 define("LAN_MAILOUT_206", "Onjuist mail ID (--ID--) gespecificeerd");
 define("LAN_MAILOUT_207", "Template laad fout");
 define("LAN_MAILOUT_208", "Template conversie fout");
+
 define("LAN_MAILOUT_209", "");
 define("LAN_MAILOUT_210", "");
 
