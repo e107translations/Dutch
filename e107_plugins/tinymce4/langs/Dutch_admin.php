7595c1b0a9794f915637be8e766132cbcfa77101
leo <the_guardian2@hotmail.com>
2017-11-09 22:05:03 +0100

    Update Dutch_admin.php
    09-11-2017

--- e107_plugins/tinymce4/langs/Dutch_admin.php
+++ e107_plugins/tinymce4/langs/Dutch_admin.php
@@ -14,4 +14,5 @@ define("TMCEALAN_2", "Browser spellingscontrole");
 define("TMCEALAN_3", "Gebruik dit wanneer de interne spellingscontrole van de browser moet worden gebruikt.");
 define("TMCEALAN_4", "Visuele Blokken");
 define("TMCEALAN_5", "Activeer dit om html blokken zichtbaar te maken tijdens het wijzigen.");
+define("TMCEALAN_6", "Code-Highlight CSS klasse.");
 ?>
