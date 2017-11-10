8008b431ead936b99fc0f74f135f1130cbabb7d3
leo <the_guardian2@hotmail.com>
2017-11-10 18:03:51 +0100

    Create Dutch_admin.php
    missing

---
+++ e107_plugins/tinymce/languages/Dutch_admin.php
@@ -0,0 +1,18 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system English Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/11/10
+|
+|        $Author: Yvalni & Tgtje $
++---------------------------------------------------------------+
+*/
+define("TMCEALAN_1", "Plak als tekst is standaard");
+define("TMCEALAN_2", "Browser spellingscontrole");
+define("TMCEALAN_3", "Gebruik dit wanner de interne spellingscontrole van de browser moet worden gebruikt.");
+define("TMCEALAN_4", "Visuele Blokken");
+define("TMCEALAN_5", "Activeer dit om html blokken zichtbaar te maken tijdens het wijzigen.");
+define("TMCEALAN_6", "Code-Highlight CSS klasse.");
+?>
