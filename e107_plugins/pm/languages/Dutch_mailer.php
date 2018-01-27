1a485c29b535ccbcc65fff40585f0defac996b5d
leo <the_guardian2@hotmail.com>
2018-01-27 18:22:52 +0100

    Update Dutch_mailer.php

--- e107_plugins/pm/languages/Dutch_mailer.php
+++ e107_plugins/pm/languages/Dutch_mailer.php
@@ -15,7 +15,7 @@ define("LAN_EC_PM_02", "");
 define("LAN_EC_PM_03", "");
 define("LAN_EC_PM_04", "PM Handler");
 define("LAN_EC_PM_05", "Verwerk grote aantallen gestuurde PMs");
-define("LAN_EC_PM_06", "Start bulk PM verwerking voor --NUM-- ontvangers ");
+define("LAN_EC_PM_06", "Start bulk PM verwerking voor [y] ontvangers ");
 define("LAN_EC_PM_07", "");
 
 
