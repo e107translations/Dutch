5ce57f04a4fa4f2a5ed61953dadab52fe5ccb220
leo <the_guardian2@hotmail.com>
2018-07-21 11:47:18 +0200

    Update lan_ugflag.php
    versienr

--- e107_languages/Dutch/admin/lan_ugflag.php
+++ e107_languages/Dutch/admin/lan_ugflag.php
@@ -17,7 +17,7 @@ define("UGFLAN_3", "Bijwerken Onderhoudsinstellingen ");
 define("UGFLAN_4", "Onderhoudsinstellingen ");
 
 define("UGFLAN_5", "Tekst die getoond wordt als de site down is");
-define("UGFLAN_6", "Laat leeg om de standaardmelding weer te geven");
+define("UGFLAN_6", "Laat leeg om de standaard melding weer te geven");
 define("UGFLAN_8", "Alleen toegang voor beheerder(s)");
 define("UGFLAN_9", "Alleen toegang voor hoofdbeheerder(s)");
 ?>
