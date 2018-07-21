abecefeb45507e9bc2bbe63a6d1b169af44f1fd2
leo <the_guardian2@hotmail.com>
2018-07-21 11:45:44 +0200

    Update lan_plugin.php
    versienr en whitespace

--- e107_languages/Dutch/admin/lan_plugin.php
+++ e107_languages/Dutch/admin/lan_plugin.php
@@ -305,5 +305,4 @@ define("LAN_PLUGIN_IS_USED", "[x] plugin wordt gebruikt door:");
 define("EPL_ADLAN_254", "Dit zal jouw taal plugin bestanden controleren op fouten en op algemene of dubbel voorkomende LAN definities. ");
 define("EPL_ADLAN_255", "Overschrijf bestanden");
 define("EPL_ADLAN_256", "[x] Overgeslagen (bestaat reeds)");
-
 ?>
