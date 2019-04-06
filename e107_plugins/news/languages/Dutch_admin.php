15ff7589426b3912e4eba7521f258e29f9eb437b
leo <the_guardian2@hotmail.com>
2019-04-06 22:15:07 +0200

    Update Dutch_admin.php

--- e107_plugins/news/languages/Dutch_admin.php
+++ e107_plugins/news/languages/Dutch_admin.php
@@ -13,7 +13,7 @@
 define("LAN_NEWS_ADMIN_00", "Laatste Nieuws Items");
 define("LAN_NEWS_ADMIN_01", "Sticky Nieuws Items");
 define("LAN_NEWS_ADMIN_02", "Toegewezen Nieuws items");
-define("LAN_NEWS_ADMIN_03", "Limit news items to a specific category");
+define("LAN_NEWS_ADMIN_03", "Limiteer nieuws berichten toe aan een specifieke categorie");
 define("LAN_NEWS_ADMIN_04", "Toegewezen berichten zijn berichten die met een template zijn toegewezen aan het 'News Grid Menu'");
 define("LAN_NEWS_ADMIN_05", "Aantal berichten wat getoond wordt");
 define("LAN_NEWS_ADMIN_06", "Titel karakters limiet");
