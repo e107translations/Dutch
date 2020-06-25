0d0104f3a12e23c5fb441d65fe44d11c135a9376
leo <the_guardian2@hotmail.com>
2020-06-25 17:21:04 +0200

    Update lan_phpinfo.php
    textual

--- e107_languages/Dutch/admin/lan_phpinfo.php
+++ e107_languages/Dutch/admin/lan_phpinfo.php
@@ -13,7 +13,7 @@
 define("PHP_LAN_1", "Als je CURL hebt geinstalleerd op de server moet je overwegen om dit uit te zetten.");
 define("PHP_LAN_2", "Dit geeft namelijk een veiligheidsrisico en is niet noodzakelik voor e107.");
 define("PHP_LAN_3", "Op een produktieserver is het beter om foutmeldingen niet te tonen in een browser.");
-define("PHP_LAN_4", "Wanneer dit is uitgeschakeld zie je ook niet de PHP versie niet meer in je browser.");
+define("PHP_LAN_4", "Wanneer dit is uitgeschakeld zie je ook niet de PHP versie in je browser.");
 define("PHP_LAN_5", "Dit is een veilgheidsrisico en zou uitgezet moeten worden.");
 define("PHP_LAN_6", "[b]session.save_path[/b] is niet schrijfbaar! Dit kan tot grote problemen leiden binnen je website.");
 define("PHP_LAN_7", "PHP Configuratie probleem(=blemen) gevonden:");
