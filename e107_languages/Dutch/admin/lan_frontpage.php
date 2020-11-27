deb19162a7722252d78ceb83444e5ffe5ec84d4c
leo <the_guardian2@hotmail.com>
2020-11-27 14:14:15 +0100

    Update lan_frontpage.php
    defineS commented

--- e107_languages/Dutch/admin/lan_frontpage.php
+++ e107_languages/Dutch/admin/lan_frontpage.php
@@ -11,7 +11,7 @@
 */
 
 define("FRTLAN_13", "Instellingen Hoofdpagina");
-define("FRTLAN_15", "Overig (invoeren url):");
+//define("FRTLAN_15", "Overig (invoeren url):");
 
 define("FRTLAN_30", "Maatwerk pagina");
 define("FRTLAN_35", "Pagina na inloggen");
@@ -19,12 +19,12 @@ define("FRTLAN_42", "Voeg nieuwe regel toe");
 define("FRTLAN_43", "Klasse");
 
 define("FRTLAN_46", "Wijzig bestaande regel");
-define("FRTLAN_47", "Naar boven");
-define("FRTLAN_48", "Naar onder");
+//define("FRTLAN_47", "Naar boven");
+//define("FRTLAN_48", "Naar onder");
 define("FRTLAN_49", "Home Pagina");
-define("FRTLAN_50", "(Om uit te zetten, selecteer &quot;Anders&quot; met een lege URL)");
+//define("FRTLAN_50", "(Om uit te zetten, selecteer &quot;Anders&quot; met een lege URL)");
 define("FRTLAN_51", "Anders");
-define("FRTLAN_53", "Gebruikers klasse");
+//define("FRTLAN_53", "Gebruikers klasse");
 
 define("FRTLAN_PAGE_TITLE", "Startpagina");
 define("FRTLAN_56", "dupliceren definitie voor de klasse:");
