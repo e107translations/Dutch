b2531a249b60198a2e1c1ccb56ea01b4a8715a01
leo <the_guardian2@hotmail.com>
2021-01-04 11:44:44 +0100

    Update lan_signup.php
    new

--- e107_languages/Dutch/lan_signup.php
+++ e107_languages/Dutch/lan_signup.php
@@ -122,6 +122,7 @@ define("LAN_SIGNUP_121", "Gebruik een ander e-mail adres");
 define("LAN_SIGNUP_122", "Privacyverklaring ");
 define("LAN_SIGNUP_123", "Algemene Voorwaarden");
 define("LAN_SIGNUP_124", "Door u aan te melden gaat u akkoord met onze [x] en onze [y].");
+define("LAN_SIGNUP_125", "Min. [x] karakters.");
 
 /*
 define("LAN_7", "Gebruikersnaam: ");
