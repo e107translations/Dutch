aa86c926f56416f9da73d8554e704a45bf05c337
leo <the_guardian2@hotmail.com>
2021-02-05 21:28:17 +0100

    05-02-2021

--- e107_plugins/newsletter/languages/Dutch.php
+++ e107_plugins/newsletter/languages/Dutch.php
@@ -12,8 +12,8 @@
 
 define("NLLAN_MENU_CAPTION", "Nieuwsbrief");
 
-define("NLLAN_48", "Je bent geabonneerd op deze nieuwsbrief. Als je je af wilt melden, klik dan op de onderstaande knop.");
-define("NLLAN_49", "Wil je je echt afmelden voor deze nieuwsbrief?");
+//define("NLLAN_48", "Je bent geabonneerd op deze nieuwsbrief. Als je je af wilt melden, klik dan op de onderstaande knop.");
+//define("NLLAN_49", "Wil je je echt afmelden voor deze nieuwsbrief?");
 define("NLLAN_50", "Klik om te abonneren (je adres luidt");
 define("NLLAN_51", "Afmelden");
 define("NLLAN_52", "Abonneren");
