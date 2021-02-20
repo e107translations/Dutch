38bed719d873971cccf86dd9948db77dadf2f5fd
leo <the_guardian2@hotmail.com>
2021-02-20 13:50:42 +0100

    Update lan_meta.php

--- e107_languages/Dutch/admin/lan_meta.php
+++ e107_languages/Dutch/admin/lan_meta.php
@@ -16,4 +16,8 @@ define("METLAN_00", "Meta Tags");
 define("METLAN_1", "Extra meta-tags");
 define("METLAN_2", "vb. < meta name='revisit-after' content='30 days' />"); //FIXME space between < and meta: parses meta tag for some reason
 define("METLAN_3", "Gebruik Nieuwstitel en de samenvatting als de meta-beschrijving op nieuwspagina´s.");
+define("METLAN_4", "Aangepaste tags (binnenin [x] tags)");
+define("METLAN_5", "Aangepaste tags (na [x])");
+define("METLAN_6", "Aangepaste tags (voor [x])");
+define("METLAN_7", "Alle metagegevens of aangepaste HTML-tags die hier worden ingevoerd (zoals <script> tags of Google Analytics-code) zullen op elke pagina van de website worden opgenomen in de daarvoor bestemde gebieden.");
 
