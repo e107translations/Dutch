d7bb0bd0263616d7f70b72db77a8bbf115c8fc95
leo <the_guardian2@hotmail.com>
2021-03-18 14:40:52 +0100

    Add files via upload

---
+++ e107_plugins/hero/languages/Dutch/Dutch_global.php
@@ -0,0 +1,15 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system 
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/12/18 12:42:44
+|
+|        $Author:  $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_HERO_NAME", "Hero");
+define("LAN_PLUGIN_HERO_SUMMARY", "Home page 'Hero' gebieds management");
+define("LAN_PLUGIN_HERO_DESCRIPTION", "Een beeld en tekst slider met geanimeerde bullet points voor het hero gebied van uw home page.");
