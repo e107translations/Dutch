c7e38a9e44d7bda841007c3b0d36d828787360a4
leo <the_guardian2@hotmail.com>
2021-04-16 12:49:16 +0200

    Update lan_contact.php
    just in case

--- e107_languages/Dutch/lan_contact.php
+++ e107_languages/Dutch/lan_contact.php
@@ -9,7 +9,34 @@
 |     $Author: Yvalni, Tgtje $
 +----------------------------------------------------------------------------+
 */
+//define("PAGE_NAME",     "Contact Pagina");
+define('LAN_CONTACT_00', 'Contact ');// contact met ons? of opnemen met ons? lijkt dit gewoon het simpelste vlgs code met enkele '
+define("LAN_CONTACT_01", "Contact details");
+define("LAN_CONTACT_02", "Contactformulier");
+define("LAN_CONTACT_03", "Naam:");
+define("LAN_CONTACT_04", "E-mail:");
+define("LAN_CONTACT_05", "Onderwerp:");
+define("LAN_CONTACT_06", "Type je bericht:");
+define("LAN_CONTACT_07", "E-mail een kopie van dit bericht naar je eigen adres ");
+define("LAN_CONTACT_08", "Verstuur");
+define("LAN_CONTACT_09", "Hartelijk dank voor je bericht. We nemen zo snel mogelijk contact op.");
+define("LAN_CONTACT_10", "Er was een probleem bij het versturen.");
+define("LAN_CONTACT_11", "Je e-mailadres lijkt niet juist.\\nControleer het en probeer het nogmaals.");
+define("LAN_CONTACT_12", "Het bericht is te kort.");
+define("LAN_CONTACT_13", "Geef een onderwerp op.");
+define("LAN_CONTACT_14", "Stuur bericht aan:");
+define("LAN_CONTACT_15", "Onjuiste code ingevoerd");
+define("LAN_CONTACT_16", "Je dient te zijn [geregistreerd] en zijn ingelogd om dit te kunnen gebruiken"); //[register]
+define("LAN_CONTACT_17", "Geef hier je naam in.");
+define("LAN_CONTACT_18", "Geef je e-mailadres in.");
+define("LAN_CONTACT_19", "Geef het onderwerp voor je mail in.");
+define("LAN_CONTACT_20", "Geef hier je bericht in.");
+define("LAN_CONTACT_21", "Ik ga ermee akkoord dat mijn informatie wordt opgeslagen om op mijn vraag te kunnen reageren."); //   By using this form, you agree to the storage and processing of your data through this site.
+define("LAN_CONTACT_22", "Privacy verklaring");
+define("LAN_CONTACT_23", "Je kan onze privacy verklaring hier lezen: [x]");
+define("LAN_CONTACT_24", "AVG toestemming");
 
+/* if bc failsactivate this below
 //define("PAGE_NAME",     "Contact Pagina");
 define('LANCONTACT_00', 'Contact ');// contact met ons? of opnemen met ons? lijkt dit gewoon het simpelste vlgs code met enkele '
 define("LANCONTACT_01", "Contact details");
@@ -36,4 +63,5 @@ define("LANCONTACT_21", "Ik ga ermee akkoord dat mijn informatie wordt opgeslage
 define("LANCONTACT_22", "Privacy verklaring");
 define("LANCONTACT_23", "Je kan onze privacy verklaring hier lezen: [x]");
 define("LANCONTACT_24", "AVG toestemming");
+*/
 
