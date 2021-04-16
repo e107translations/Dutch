446a3e31aa556e136115086dcf37b5e65bc39250
leo <the_guardian2@hotmail.com>
2021-04-16 13:20:42 +0200

    Update Dutch.php
    fix

--- e107_plugins/newsletter/languages/Dutch.php
+++ e107_plugins/newsletter/languages/Dutch.php
@@ -12,8 +12,8 @@
 
 define("NLLAN_MENU_CAPTION", "Nieuwsbrief");
 
-//define("NLLAN_48", "Je bent geabonneerd op deze nieuwsbrief. Als je je af wilt melden, klik dan op de onderstaande knop.");
-//define("NLLAN_49", "Wil je je echt afmelden voor deze nieuwsbrief?");
+define("NLLAN_48", "Je bent geabonneerd op deze nieuwsbrief. Als je je af wilt melden, klik dan op de onderstaande knop.");
+define("NLLAN_49", "Wil je je echt afmelden voor deze nieuwsbrief?");
 define("NLLAN_50", "Klik om te abonneren (je adres luidt");
 define("NLLAN_51", "Afmelden");
 define("NLLAN_52", "Abonneren");
@@ -23,7 +23,7 @@ define("NLLAN_67", "Archief overzicht");
 define("NLLAN_68", "Invalide parameter gedefinieerd");
 define("NLLAN_69", "Geen verzonden nieuwsbrieven beschikbaar.");
 define("NLLAN_70", "Geselecteerde nieuwsbrief bestaat niet");
-define("NLLAN_71", "Terug");
+//define("NLLAN_71", "Terug");
 define("NLLAN_72", "Bekijk archief");
 
 define("NLLAN_73", "Geef je e-mail adres");
