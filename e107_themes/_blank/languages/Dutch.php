9c91f09a0d7f49ee23b71bc23c11c3cbd9d72c01
leo <the_guardian2@hotmail.com>
2021-12-04 12:54:47 +0100

    D_LP 2.3.1

---
+++ e107_themes/_blank/languages/Dutch.php
@@ -0,0 +1,13 @@
+<?php
+define("EMESSLAN_TITLE_INFO", "Systeem Informatie");
+//define("EMESSLAN_TITLE_ERROR", "Error");//new > LAN_ERROR
+define("EMESSLAN_TITLE_SUCCESS", "Succes");
+define("EMESSLAN_TITLE_WARNING", "Waarschuwing");
+define("EMESSLAN_TITLE_DEBUG", "Systeem Debug");
+
+define("LAN_THEME_1", "Commentaren voor dit item zijn uitgeschakeld");
+define("LAN_THEME_2", "Lees/Plaats Commentaar: ");
+define("LAN_THEME_3", "Lees meer...");
+// define("LAN_THEME_4", "Trackbacks: ");
+define("LAN_THEME_5", "Geplaatst door ");
+define("LAN_THEME_6", "op ");
