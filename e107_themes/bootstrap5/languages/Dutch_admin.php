c8c0392f3c51e68c2a91ea20ea0827a40988d459
leo <the_guardian2@hotmail.com>
2022-04-11 11:13:35 +0200

    v 2.3.2

---
+++ e107_themes/bootstrap5/languages/Dutch_admin.php
@@ -0,0 +1,11 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - 
+|
++----------------------------------------------------------------------------+
+*/
+
+define("LAN_THEMEPREF_01", "Bootswatch Styles:");
+define("LAN_THEMEPREF_02", "Toon standaard menu met card uierlijk");
+define("LAN_THEMEPREF_03", "Toon Login Pagina als standaard pagina");
