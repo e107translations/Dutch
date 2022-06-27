1d17a9f1fd97448b435e9ec285c67b57de59e746
leo <the_guardian2@hotmail.com>
2022-06-27 11:37:58 +0200

    Update lan_meta.php

--- e107_languages/Dutch/admin/lan_meta.php
+++ e107_languages/Dutch/admin/lan_meta.php
@@ -21,3 +21,5 @@ define("METLAN_4", "Aangepaste tags (binnenin [x] tags)");
 define("METLAN_5", "Aangepaste tags (na [x])");
 define("METLAN_6", "Aangepaste tags (voor [x])");
 define("METLAN_7", "Alle metagegevens of aangepaste HTML-tags die hier worden ingevoerd (zoals <script> tags of Google Analytics-code) zullen op elke pagina van de website worden opgenomen in de daarvoor bestemde gebieden.");
+define("METLAN_8", "Aantal tekens limiet voor SEO-titel");
+define("METLAN_9", "Aantal tekens limiet voor SEO-beschrijving");
