4bcc3f5a158328e618a8e24d3bd9fbdf7e5353f4
leo <the_guardian2@hotmail.com>
2022-09-05 21:37:22 +0200

    Update lan_submitnews.php

--- e107_languages/Dutch/lan_submitnews.php
+++ e107_languages/Dutch/lan_submitnews.php
@@ -37,7 +37,7 @@ define("SUBNEWSLAN_7", "Je moet je naam en e-mailadres intypen");
 define("SUBNEWSLAN_8", "Fout tijdens het uploaden van de afbeelding(en)");
 define("SUBNEWSLAN_9", "Kernwoorden");
 //define("SUBNEWSLAN_10", "Samenvatting");
-//define("SUBNEWSLAN_11", "Meta Beschrijving");
+define("SUBNEWSLAN_11", "Meta Beschrijving");
 define("SUBNEWSLAN_12", "Gebruikt door Facebook enz.");
 define("SUBNEWSLAN_13", "Media URLs");
 
