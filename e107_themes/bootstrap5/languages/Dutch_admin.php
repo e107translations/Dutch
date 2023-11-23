96d3e989280ba8fd97f91b0272adaacb951a9a61
leo <the_guardian2@hotmail.com>
2023-11-23 16:10:05 +0100

    Update Dutch_admin.php
    nl +

--- e107_themes/bootstrap5/languages/Dutch_admin.php
+++ e107_themes/bootstrap5/languages/Dutch_admin.php
@@ -6,6 +6,12 @@
 +----------------------------------------------------------------------------+
 */
 
-define("LAN_THEMEPREF_01", "Bootswatch Styles:");
-define("LAN_THEMEPREF_02", "Toon standaard menu met card uierlijk");
-define("LAN_THEMEPREF_03", "Toon Login Pagina als standaard pagina");
+define("LAN_THEMEPREF_00", "Huisstijl/Merknaam:");
+define("LAN_THEMEPREF_01", "Uitlijning navigatiebalk:");
+define("LAN_THEMEPREF_02", "Standaardmenu weergeven met card look");
+define("LAN_THEMEPREF_03", "Geef de inlogpagina weer als standaardpagina");
+define("LAN_THEMEPREF_04", "Site Name");
+define("LAN_THEMEPREF_05", "Logo");
+define("LAN_THEMEPREF_06", "Logo &amp; Site Naam");
+define("LAN_THEMEPREF_07", "links");
+define("LAN_THEMEPREF_08", "rechts");
