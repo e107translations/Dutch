47f2fc6f21dbb394db9fb4fee1cb62958d6d5d11
leo <the_guardian2@hotmail.com>
2023-12-08 09:00:39 +0100

    2.3.x
    textual

--- e107_languages/Dutch/lan_fpw.php
+++ e107_languages/Dutch/lan_fpw.php
@@ -14,7 +14,7 @@
 	define("PAGE_NAME", "Wachtwoord vernieuwen");
 }
 
-define("LAN_02", "Sorry, mail kan niet worden verstuurd, neem contact op met de website beheerder.");
+define("LAN_02", "Sorry, e-mail kan niet worden verstuurd, neem contact op met de website beheerder.");
 define("LAN_03", "Wachtwoord vernieuwen");
 define("LAN_05", "Geef om het wachtwoord te vernieuwen de volgende informatie op");
 define("LAN_06", "Poging vernieuwen wachtwoord");
@@ -23,7 +23,7 @@ define("LAN_08", "heeft geprobeert het beheerderswachtwoord te vernieuwen.");
 define("LAN_09", "Wachtwoord vernieuwd door ");
 //define("LAN_112", "E-mail adres geregistreerd op deze website");
 //define("LAN_156", "Verstuur");
-define("LAN_213", "Deze gebruikersnaam / e-mailsdres is niet aanwezig binnen onze database.");
+define("LAN_213", "Dit e-mailsdres of deze gebruikersnaam is niet aanwezig in onze database.");
 define("LAN_214", "Kan wachtwoord niet vernieuwen");
 //define("LAN_216", "Ga naar de volgende URL om je nieuwe wachtwoord te bevestigen ...");
 //define("LAN_217", "Je nieuwe wachtwoord is nu geldig, je kunt met het nieuwe wachtwoord inloggen.");
@@ -39,7 +39,7 @@ define("LAN_FPW7", "Dit is geen geldige link om je wachtwoord te wijzigen.<br />
 define("LAN_FPW8", "Je wachtwoord is succesvol aangepast.");
 define("LAN_FPW9", "Het nieuwe wachtwoord is:");
 define("LAN_FPW10", "Alstublieft");
-define("LAN_FPW11", "log nu in");
+define("LAN_FPW11", "Log nu in");
 define("LAN_FPW12", "en wijzig om veiligheids redenen direct je wachtwoord.");
 define("LAN_FPW13", "volg de instructies in de e-mail om je wachtwoord te bevestigen.");
 define("LAN_FPW14", "is aangevraagd door iemand met het ip-adres");
@@ -47,11 +47,11 @@ define("LAN_FPW15", "Dat betekent nog niet dat je wachtwoord nu al is gewijzigd.
 define("LAN_FPW16", "Als je het niet zelf aanvroeg en je het wachtwoord NIET wilt wijzigen, negeer dan dit bericht.");
 define("LAN_FPW17", "De onderstaande link is 48 uur geldig.");
 define("LAN_FPW18", "Nieuw wachtwoord aangevraagd");
-define("LAN_FPW19", "Kon geen e-mail versturen");
+define("LAN_FPW19", "E-mail versturen mislukt");
 define("LAN_FPW20", "E-mail verstuurd");
 define("LAN_FPW21", "Gebruiker heeft op de wachtwoord reset link geklikt");
 define("LAN_FPW22", "E-mail adres geregistreerd op deze website");
 define("LAN_FPW_100", "Wachtwoord vergeten?");
-define("LAN_FPW_101", "Geen probleem. Geef je e-mailadres hieronder in,en we sturen je een email met instructies om het wachtwoord te wijzigen.");
+define("LAN_FPW_101", "Geef je e-mailadres in,en je ontvangt een mail met instructies om je wachtwoord te wijzigen.");
 define("LAN_FPW_102", "Reset wachtwoord");
 
