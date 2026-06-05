f42f8f16ba3d1a1768e90729b60e06cb1e7232cd
leo <the_guardian2@hotmail.com>
2026-06-05 12:10:07 +0200

    2.3.6
    last define mode

---
+++ e107_plugins/languages/Dutch/admin_e107db_conf.php
@@ -0,0 +1,19 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Dutch Language File.
+|
+|     $URL:$
+|     $Revision:$
+|     $Date:$
+|     $Author: Yvalni, Tgtje $
++----------------------------------------------------------------------------+
+*/
+
+define("E107DB_LAN_1", "e107 formaat database");
+define("E107DB_LAN_9", "Wachtwoord methode");
+define("E107DB_LAN_10", "Configureer e107 db auth");
+define("E107DB_LAN_11", "Vink box aan voor elk veld, dat u wenst te worden overgedragen aan de lokale database:");
+define("IMPORTDB_LAN_7", "MD5 (e107 origineell)");
+define("IMPORTDB_LAN_8", "e107 salted (optie 2.0 aan)");
+define("LAN_AUTHENTICATE_HELP", "Deze authenticatiemethode wordt gebruikt met een tweede e107 databank die een andere wachtwoord indeling kan gebruiken dan dit systeem. Het oorspronkelijke wachtwoord wordt gelezen uit de lokale database en gevalideerd tegen het opslagformaat van het oorspronkelijke systeem. Indien juist geverifieert, zal het worden omgezet in het huidige e107-compatibel formaat en opgeslagen worden in de database.");
