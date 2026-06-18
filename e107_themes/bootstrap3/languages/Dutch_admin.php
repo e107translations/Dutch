116b31da857591ff92a5314c0a04644a8158f108
Alex <info@exas.nl>
2026-06-18 14:27:32 +0200

    Update Dutch_admin.php
    Aangepast

--- e107_themes/bootstrap3/languages/Dutch_admin.php
+++ e107_themes/bootstrap3/languages/Dutch_admin.php
@@ -10,13 +10,13 @@
 +----------------------------------------------------------------------------+
 */
 define("LAN_THEMEPREF_00", "Huisstijl/Merknaam:");
-define("LAN_THEMEPREF_01", "Uitlijning Navigatiebalk:");
+define("LAN_THEMEPREF_01", "Uitlijning navigatiebalk:");
 define("LAN_THEMEPREF_02", "Login/Aanmelding positie");
-define("LAN_THEMEPREF_03", "Bootswatch Stijlen:");
-define("LAN_THEMEPREF_04", "Site Naam");
+define("LAN_THEMEPREF_03", "Bootswatch stijlen:");
+define("LAN_THEMEPREF_04", "Site naam");
 define("LAN_THEMEPREF_05", "Logo");
-define("LAN_THEMEPREF_06", "Logo &amp; Site Name");
-define("LAN_THEMEPREF_07", "left");
-define("LAN_THEMEPREF_08", "right");
-define("LAN_THEMEPREF_09", "top");
-define("LAN_THEMEPREF_10", "bottom");
+define("LAN_THEMEPREF_06", "Logo &amp; Site naam");
+define("LAN_THEMEPREF_07", "links");
+define("LAN_THEMEPREF_08", "rechts");
+define("LAN_THEMEPREF_09", "boven");
+define("LAN_THEMEPREF_10", "onder");
