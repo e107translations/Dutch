81a972a50e10aa05f44df20b7754afb930acf69b
Alex <info@exas.nl>
2026-06-18 14:24:35 +0200

    Update Dutch_admin.php
    Wie?

--- e107_plugins/tinymce4/langs/Dutch_admin.php
+++ e107_plugins/tinymce4/langs/Dutch_admin.php
@@ -6,12 +6,12 @@
 |        GNU General Public License (http://gnu.org).
 |        Last Modified: 2017/02/18 11:38:44
 |
-|        $Author: Yesszus $
+|        $Author: Yvalni & tgtje $
 +---------------------------------------------------------------+
 */
 define("TMCEALAN_1", "Plak als tekst is standaard");
 define("TMCEALAN_2", "Browser spellingscontrole");
-define("TMCEALAN_3", "Gebruik dit wanneer de interne spellingscontrole van de browser moet worden gebruikt.");
+define("TMCEALAN_3", "Gebruik dit wanner de interne spellingscontrole van de browser moet worden gebruikt.");
 define("TMCEALAN_4", "Visuele Blokken");
 define("TMCEALAN_5", "Activeer dit om html blokken zichtbaar te maken tijdens het wijzigen.");
 define("TMCEALAN_6", "Code-Highlight CSS klasse.");
