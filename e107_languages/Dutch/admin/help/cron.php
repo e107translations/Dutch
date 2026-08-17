ff5626e4975cad69369fba40ae0b139ac98133c4
leo <the_guardian2@hotmail.com>
2026-08-17 13:51:59 +0200

    Update cron.php
    file deleted (nulled) 17-08-2026 cron update

--- e107_languages/Dutch/admin/help/cron.php
+++ e107_languages/Dutch/admin/help/cron.php
@@ -11,7 +11,8 @@
 */
 
 
-
+/* file deleted (nulled) 17-08-2026 cron update
 $mes = e107::getMessage();
 $mes->setTitle(defset('LAN_STATUS'), 'info');
 echo $mes->render('default','info', false);
+*/
