41022cdcee5fa31f787c5d020f0e4b644f33feb3
leo <the_guardian2@hotmail.com>
2026-08-31 13:23:26 +0200

    Update lan_installer.php
    mmhh bc? 2.3.11

--- e107_languages/Dutch/lan_installer.php
+++ e107_languages/Dutch/lan_installer.php
@@ -124,3 +124,4 @@ define("LANINS_147", "Beheer");
 define("LANINS_148", "De MySQL-verbindingsgegevens mogen geen enkele aanhalingstekens (') of puntkomma's (;) bevatten.");
 define("LANINS_149", "Het tabelvoorvoegsel mag alleen letters, cijfers en underscores (_) bevatten.");
 define("LANINS_150", "De versie van MySQL die op uw server is geinstalleerd, voldoet niet aan de minimale vereisten van [x]. Upgrade MySQL of neem contact op met uw hostingprovider.");
+define("LANINS_151", "Veilige willekeurige nummergenerator"); //bc?
