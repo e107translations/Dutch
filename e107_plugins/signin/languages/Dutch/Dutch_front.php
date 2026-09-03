4f6d1e9208b078a12ab2a95f5f63993d1bdf19e9
Alex <info@exas.nl>
2026-09-03 08:18:09 +0200

    Create Dutch_front.php
    Kan dit zo tgtje of moeten het weer defines worden in de vertaling?

---
+++ e107_plugins/signin/languages/Dutch/Dutch_front.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * e107 website system
+ *
+ * Copyright (C) 2008-2026 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Signin Plugin Frontend Language Definitions
+ *
+*/
+
+
+return [
+	'LAN_SIGNIN_USERNAME'    => "Inlognaam:",
+	'LAN_SIGNIN_EMAIL'       => "E-mail:",
+	'LAN_SIGNIN_USEREMAIL'   => "Inlognaam of e-mail:",
+	'LAN_SIGNIN_SIGNIN'      => "Inloggen",
+	'LAN_SIGNIN_SIGNUP'      => "Aanmelden",
+	'LAN_SIGNIN_REMEMBER'    => "Vergeet me niet",
+	'LAN_SIGNIN_FPW'         => "Wachtwoord vergeten?",
+	'LAN_SIGNIN_RESEND'      => "Activeringsmail verzenden",
+	'LAN_SIGNIN_PROFILE'     => "Profiel",
+	'LAN_SIGNIN_ADMIN'       => "Beheer",
+	'LAN_SIGNIN_MAINTENANCE' => "De onderhoudsvlag is gezet - dit betekent dat gewone gebruikers naar sitedown.php worden geleid. Om de vlag terug te zetten ga naar beheer/onderhoud.",
+];
