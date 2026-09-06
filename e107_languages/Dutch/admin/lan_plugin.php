411816c62bd1fccaefcf115e37d6360149cc5c38
Alex <info@exas.nl>
2026-09-06 14:44:02 +0200

    Update lan_plugin.php

--- e107_languages/Dutch/admin/lan_plugin.php
+++ e107_languages/Dutch/admin/lan_plugin.php
@@ -306,3 +306,5 @@ define("EPL_ADLAN_254", "Dit zal jouw taal plugin bestanden controleren op foute
 define("EPL_ADLAN_255", "Overschrijf bestanden");
 define("EPL_ADLAN_256", "[x] Overgeslagen (bestaat reeds)");
 define("EPL_ADLAN_257","Alleen lezen");
+define("EPL_ADLAN_REFUSED_PULL_TOKEN_MISSING","De plugin is niet bijgewerkt vanuit de Git-repository, omdat de link geen beveiligingstoken bevatte. Start de plugin via de Plugin Manager in plaats van via een bladwijzer of een link op een andere website.");
+define("EPL_ADLAN_REFUSED_BUILD_TOKEN_MISSING","Er is geen tabeldefinitiebestand geschreven, omdat de link geen beveiligingstoken bevatte. Start de pluginbuilder vanuit het menu Plugins in plaats van via een bladwijzer of een link op een andere site.");
