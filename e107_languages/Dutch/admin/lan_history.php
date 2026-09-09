78e6f32599cdd691689946536650f51c6b142324
leo <the_guardian2@hotmail.com>
2026-09-09 11:29:42 +0200

    new file
    Master only

---
+++ e107_languages/Dutch/admin/lan_history.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Copyright (C) 2008-2026 e107 Inc (e107.org), Licensed under GNU GPL (http://www.gnu.org/licenses/gpl.txt)
+ *
+ * Admin History.Language File
+ *
+*/
+
+return [
+    'LAN_HISTORY_TABLE' => "Tabel",
+    'LAN_HISTORY_ACTION' => "Actie",
+    'LAN_HISTORY_CHANGED_DATA' => "Gewijzigde Data",
+    'LAN_HISTORY_RESTORE_COLUMN' => "Hersteld",
+    'LAN_HISTORY_RESTORE' => "Herstellen",
+    'LAN_HISTORY_RESTORE_RECORD_BUTTON' => "Herstel dit record",
+    'LAN_HISTORY_HELP_INTRO' => "Op deze pagina kunt u de <strong>wijzigingsgeschiedenis</strong> van de records in het systeem bekijken en records indien nodig terugzetten naar een eerdere staat.",
+    'LAN_HISTORY_HELP_FEATURES' => "Kenmerken van deze pagina:",
+    'LAN_HISTORY_HELP_VIEW' => "<strong>Wijzigingen bekijken:</strong> Bekijk de details van aanpassingen en verwijderingen, inclusief wie de wijzigingen heeft aangebracht en wanneer.",
+    'LAN_HISTORY_HELP_REVERT' => "<<strong>Wijzigingen ongedaan maken:</strong> Een record terugzetten naar een eerdere versie, waardoor onbedoelde of ongewenste wijzigingen worden teruggedraaid.",
+    'LAN_HISTORY_HELP_AUDIT' => "<strong>Audit trail:</strong> Houd alle handelingen bij die op records worden uitgevoerd, met het oog op verantwoordingsplicht en transparantie.",
+    'LAN_HISTORY_HELP_FILTERS' => "Gebruik de filters om de logbestanden te verfijnen of specifieke wijzigingen op te sporen. Als een record kan worden hersteld, verschijnt er een optie in het menu Opties.",
+];
