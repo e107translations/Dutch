545483f37568e4a1ce3288d810ced13cf59b5d5a
leo <the_guardian2@hotmail.com>
2026-09-20 10:08:27 +0200

    Update Dutch_admin.php

--- e107_plugins/banner/languages/Dutch_admin.php
+++ e107_plugins/banner/languages/Dutch_admin.php
@@ -51,7 +51,7 @@ define("BNRLAN_50", "Lokaal");
 define("BNRLAN_51", "Op afstand");
 define("BNRLAN_HELP_IMPRESSIONS", "0 = Onbeperkt");
 define("BNRLAN_HELP_KEYWORDS", "Wanneer nieuws of pagina's worden geladen, beperkt dit de bannerresultaten tot overeenkomende zoekwoorden. Wees hier voorzichtig mee.");
-//define("BNRLAN_HELP_01", "Banner Hulp");
+define("BNRLAN_HELP_01", "Banner Hulp");
 define("BNRLAN_HELP_02", "Gebruik deze plugin om advertenties op je website te beheren.[br][br]Met het opmaken van een nieuwe banner kan je een gebruikersnaam en wachtwoord aanmaken voor je klant om in te loggen en de voortgang van de campagne te volgen.[br][br]Je kan de banner activeren en weergeven via het menu beheer en via die pagina verder configureren.");
 define("BNRLAN_TITLE_REMOTE_IMAGE", "Dit overschrijft elke lokale afbeelding die je hebt ingesteld.");
 define("BNRLAN_MENU_MANAGER_CONFIG", "Het menu is nu geconfigureerd in de menu-manager.");
